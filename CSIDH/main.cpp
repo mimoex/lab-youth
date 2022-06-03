447ab4f13e892d618eaf4964148d80429d9f30d8
mimoex <nijidream@protonmail.com>
2022-06-03 11:26:45 +0900

    無限遠点のときにInfになるようにした

--- CSIDH/main.cpp
+++ CSIDH/main.cpp
@@ -8,7 +8,7 @@ void gen_csidh_key()
 	random_device rd;
 	default_random_engine eng(rd());
 	uniform_int_distribution<int> distr(0, 10);  //0から10までの乱数を生成 //論文では-5から5?
-	
+
 	size_t a[N], b[N]; //Aさんの鍵,Bさんの鍵
 
 	for (i = 0; i < N; i++) {
@@ -34,15 +34,15 @@ void gen_csidh_key()
 
 void ec_add_check()
 {
-	mpz_class a = 7, mod= 97;
+	mpz_class a = 7, mod = 97;
 	Point p, q, result;
-	
+
 	p.x = 24; p.y = 11;
 	q.x = 96; q.y = 34;
 	p.inf = false; q.inf = false; result.inf = false;
-	result=ec_add(p, q, a, mod);
+	result = ec_add(p, q, a, mod);
 
-	cout << "[" << p.x <<","<< p.y<<"]+[" << q.x <<","<<q.y<<"]=["<< result.x<<","<<result.y<<"]" << endl;
+	cout << "[" << p.x << "," << p.y << "]+[" << q.x << "," << q.y << "]=[" << result.x << "," << result.y << "]" << endl;
 }
 
 
@@ -57,39 +57,40 @@ Point l_to_r_binary(Point& p, const mpz_class& a, const mpz_class& mod, const mp
 	bit = n.get_str(2);
 	bit_size = bit.size();
 
-	cout << "n=" << n <<" = 0b"<< bit << endl;
+	cout << "n=" << n << " = 0b" << bit << endl;
 
 	result = p;
 
-	for (int i = bit_size-2; i >=0; i--) {
+	for (int i = bit_size - 2; i >= 0; i--) {
 		result = ec_add(result, result, a, mod);	//double
 
 		if (bit[bit_size - i - 1] == '1') {
 			result = ec_add(result, temp_point, a, mod);	//add
 		}
-
-				
 	}
 	return result;
-	
 }
 
 void binary_check()
 {
-	mpz_class a = 1, mod;
-	mpz_class n = 3;
+	mpz_class a = 1, mod, n;
 	Point p, result;
 
-	//mod=gen_prime();
-	mod = 97;
-	cout << "p="<<mod << endl;
+	mod = 4093;
+	cout << "p=" << mod << "\n" << endl;
 
-	p.x = 31; p.y = 72;
-	p.inf = false; result.inf = false;
-	for (n = 0; n < 10; n++) {
+	p.x = 2116; p.y = 4041; p.inf = false;
+	result.inf = true;
+
+	/*ループで確認 5000回*/
+	for (n = 1; n <= 5000; n++) {
 		result = l_to_r_binary(p, a, mod, n);
-		cout << "nP=" << n << "[" << p.x << ", " << p.y << "] = [" << result.x << ", " << result.y << "]\n" << endl;
-
+		if (result.inf) {
+			cout << "Inf\n"<< endl;
+		}
+		else {
+			cout << n <<"P=[" << result.x << ", " << result.y << "]\n" << endl;
+		}
 	}
 
 }
@@ -104,5 +105,6 @@ int main()
 	binary_check();
 
 	//gen_csidh_key();
+
 
 }
