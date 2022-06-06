f998945e288a2edb862d99baf6cb5cee1b76b255
mimoex <nijidream@protonmail.com>
2022-06-06 21:32:23 +0900

    CSIDHに必要なパラメータをheaderに移動

--- CSIDH/fp.h
+++ CSIDH/fp.h
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+const size_t N = 74;	//log(11^74)/log(2) ≒ 255.9979
+
+const int prime[N] = { 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 587 };
+
+
 struct Point {
 	mpz_class x, y;
 	bool inf = false;
@@ -24,3 +29,5 @@ void inv_fp(const mpz_class& a, const mpz_class& p, mpz_class** c);
 void div_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c);
 
 Point ec_add(Point& p, Point& q, mpz_class& a, mpz_class& mod);
+
+mpz_class gen_prime();
