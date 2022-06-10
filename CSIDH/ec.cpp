453d8f3bc1c9fa268bd9b2cf41bceea406f92a95
mimoex <nijidream@protonmail.com>
2022-06-10 12:04:39 +0900

    Montgomery曲線上の点の加算・2倍算の実装

--- CSIDH/ec.cpp
+++ CSIDH/ec.cpp
@@ -3,8 +3,8 @@
 
 Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
 {
-	if (p.inf) { return q; cout << "p is inf" << endl; }
-	if (q.inf) { return p; cout << "q is inf" << endl; }
+	if (p.inf) return q;
+	if (q.inf) return p;
 
 	mpz_class lambda_temp, y_1, lambda;
 	mpz_class lh, rh;
@@ -12,7 +12,7 @@ Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
 
 	if (p.x == q.x) {
 		//P + (-P) = 0
-		if (p.y == -q.y) {
+		if ((p.y + q.y == mod) || (p.y == q.y && p.y == 0)) {
 			p.inf = true;
 			return p;
 		}
@@ -24,14 +24,13 @@ Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
 		add_fp(p.y, p.y, mod, &y_1);
 
 		div_fp(lambda_temp, y_1, mod, &lambda);
-	}
-	else {
+	} else {
 		//x1!=x2のとき，
 		// cout << "Add" << endl;
 		//lambda=(y2-y1)/(x2-x1)
 		sub_fp(p.y, q.y, mod, &lh);
 		sub_fp(p.x, q.x, mod, &rh);
-		div_fp(lh, rh, mod, &lambda);	
+		div_fp(lh, rh, mod, &lambda);
 	}
 	
 	//P+Qのx座標
@@ -66,7 +65,7 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 	if (p.x == q.x) {
 		//P + (-P) = 0
 		//cout << "Py,-Py:" << p.y << "," << -q.y << endl;
-		if (p.y == -q.y) {
+		if ((p.y + q.y == mod) || (p.y == q.y && p.y == 0)) {
 			p.inf = true;
 			return p;
 		}
@@ -75,46 +74,34 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 		//
 		//x3 = b*(3*x1^2+2*a*x1+1)^2/(2*b*y1)^2-a-x1-x1
 		//A=3*x1, B=(A^2+2*a*x1+1), lh=b*B^2, C=2*b*y1, D=C^2
-		mpz_class A, A2, B, B1, C, D;
-		mul_fp(3, p.x, mod, &A);
+		mpz_class A, A2, B, B1, C, C1, D, D1;
+		pow_fp(p.x, 2, mod, &A);
+		mul_fp(3, A, mod, &A2);
+		mul_fp(2, a, mod, &B);
+		mul_fp(B, p.x, mod, &B1);
+		add_fp(A2, B1, mod, &C);
+		add_fp(C, 1, mod, &C1);
 
-		mul_fp(2, a, mod, &B1);
-		mul_fp(B1, p.x, mod, &B1);
+		mul_fp(2, b, mod, &D);
+		mul_fp(D, p.y, mod, &D1);
 
-		pow_fp(A, 2, mod, &A2);
+		div_fp(C1, D1, mod, &lambda);
 
-		add_fp(A2, B1, mod, &B);
-		add_fp(B, 1, mod, &B);
+		//x
+		mpz_class l2, a1,a2,a3,a4;
+		pow_fp(lambda, 2, mod, &l2);
+		mul_fp(b, l2, mod, &a1);
+		mul_fp(2, p.x, mod, &a2);
+		sub_fp(a1, a, mod, &a3);
+		sub_fp(a3, a2, mod, &result.x);
 
-		pow_fp(B, 2, mod, &lh);
+		//y
+		mpz_class temp10, temp11;
+		sub_fp(p.x, result.x, mod, &temp10);
+		mul_fp(lambda, temp10, mod, &temp11);
+		sub_fp(temp11, p.y, mod, &result.y);
 
 
-		mul_fp(2, b, mod, &C);
-		mul_fp(C, p.y, mod, &C);
-
-		div_fp(lh, rh, mod, &lambda);
-
-		pow_fp(lambda, 2, mod, &D);
-		mul_fp(b, D, mod, &D);
-		sub_fp(D, a, mod, &D);
-		sub_fp(rh, p.x, mod, &D);
-		sub_fp(rh, p.x, mod, &result.x);
-
-		
-
-		//y3 = (2*x1+x1+a)*(3*x1^2+2*a*x1+1)/(2*b*y1)-b*(3*x1^2+2*a*x1+1)^3/(2*b*y1)^3-y1
-		//A=2*x1, A=A+x1, A=A+a, B=A*lambda, C=lambda^3, C=b*C, D=B-C-y1
-		mul_fp(2, p.x, mod, &A);
-		add_fp(A, p.x, mod, &A);
-		add_fp(A, a, mod, &A);
-
-		mul_fp(A, lambda, mod, &B);
-
-		pow_fp(lambda, 3, mod, &C);
-		mul_fp(b, C, mod, &C);
-
-		sub_fp(B, C, mod, &D);
-		sub_fp(D, p.y, mod, &result.y);
 
 
 	}
@@ -123,43 +110,28 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 		// 
 		// x3 = b*(y2-y1)^2/(x2-x1)^2-a-x1-x2
 
-		mpz_class llh, lrh, y_ltemp, subx2x1, suby2y1;
+		sub_fp(q.y, p.y, mod, &lh);
+		sub_fp(q.x, p.x, mod, &rh);
+		div_fp(lh, rh, mod, &lambda);
 
-		sub_fp(q.x, p.x, mod, &subx2x1);	//x2-x1
-		sub_fp(q.y, p.y, mod, &suby2y1);	//y2-y1
+		mpz_class lambda2, temp1;
 
-		pow_fp(suby2y1, 2, mod, &lh);
-		mul_fp(b, lh, mod, &lh);
+		pow_fp(lambda, 2, mod, &lambda2);
+		mul_fp(b, lambda2, mod, &temp1);
+		sub_fp(temp1, a, mod, &result.x);
+		sub_fp(result.x, p.x, mod, &result.x);
+		sub_fp(result.x, q.x, mod, &result.x);
 
-		pow_fp(subx2x1, 2, mod, &rh);
-
-		div_fp(lh, rh, mod, &x_temp);
-
-		sub_fp(x_temp, a, mod, &x_temp);
-		sub_fp(x_temp, p.x, mod, &x_temp);
-		sub_fp(x_temp, q.x, mod, &result.x);
 
 
 		// y3 = (2*x1+x2+a)*(y2-y1)/(x2-x1)-b*(y2-y1)^3/(x2-x1)^3-y1
 		//
 		//左辺
-		mul_fp(2, p.x, mod, &llh);
-		add_fp(llh, q.x, mod, &llh);
-		add_fp(llh, a, mod, &llh);
-		mul_fp(llh, suby2y1, mod, &y_ltemp);
-
-		div_fp(y_ltemp, subx2x1, mod, &y_ltemp);
-
-		//右辺
-		pow_fp(suby2y1, 3, mod, &lh);
-		mul_fp(b, lh, mod, &lh);
-
-		pow_fp(subx2x1, 3, mod, &rh);
-
-		div_fp(lh, rh, mod, &lrh);
-
-		sub_fp(y_ltemp, lrh, mod, &y_ltemp);
-		sub_fp(y_ltemp, q.y, mod, &result.y);
+		mpz_class temp10, temp11;
+		sub_fp(p.x, result.x, mod, &temp10);
+		mul_fp(lambda, temp10, mod, &temp11);
+		sub_fp(temp11, p.y, mod, &result.y);
+		
 	}
 	return result;
 }
