85fde986a30e9d6f1fabc53f8e7810051e1fd4b5
mimoex <nijidream@protonmail.com>
2022-06-16 15:47:38 +0900

    modの計算で負の数が生じてしまう対策

--- CSIDH/fp.cpp
+++ CSIDH/fp.cpp
@@ -1,49 +1,52 @@
 #include "fp.h"
 
+mpz_class mod(mpz_class a, mpz_class b) {
+	return (a %= b) < 0 ? a + b : a;
+}
+
 void add_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c)
 {
 	*c = a + b;
 	if (*c >= p) *c -= p;
-	*c = *c % p;
 }
 
 void sub_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c)
 {
 	*c = (a - b);
-	*c = (*c % p + p) % p;
+	*c = mod(*c, p);
 }
 
 
 void mul_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c)
 {
 	*c = a * b;
-	*c = *c%p;
+	*c = *c % p;
 }
 
 
 void pow_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c)
 {
-	*c = a;
+	//*c = a;
 	mpz_powm(c->get_mpz_t(), a.get_mpz_t(), b.get_mpz_t(), p.get_mpz_t());
 }
 
 //逆元
-void inv_fp(const mpz_class& a, const mpz_class& p, mpz_class** c)
+void inv_fp(const mpz_class& a, const mpz_class& p, mpz_class* c)
 {
 	if (a == 0) {
 		cout << "zero inv" << endl;
-		**c = 0;
+		*c = 0;
 
 	}
 	else {
-		pow_fp(a, p - 2, p, *c);
+		pow_fp(a, p - 2, p, c);
 	}
 
 }
 
 //input  A, p; A∈[1,p-1]
 //output A^-1 mod p
-void algo5_inv(const mpz_class& A, const mpz_class& p, mpz_class** c)
+int algo5_inv(const mpz_class& A, const mpz_class& p, mpz_class* c)
 {
 	mpz_class u, v, r, s;
 
@@ -52,51 +55,54 @@ void algo5_inv(const mpz_class& A, const mpz_class& p, mpz_class** c)
 
 	if (A == 0) {
 		cout << "zero inv" << endl;
+		return -1;
 	}
 	else {
 
-	
-	while (u != 1 && v != 1) {
-		while ((u & 1) == 0) {
-			u = u / 2;
-			if ((r & 1) == 0) {
-				r = r / 2;
+
+		while (u != 1 && v != 1) {
+			while ((u & 1) == 0) {
+				u = u >>1;
+				if ((r & 1) == 0) {
+					r = r >>1;
+				}
+				else {
+					r = (r + p) >>1;
+				}
+			}
+			while ((v & 1) == 0) {
+				v = v >>1;
+				if ((s & 1) == 0) {
+					s = s >>1;
+				}
+				else {
+					s = (s + p) >>1;
+				}
+			}
+			if (u >= v) {
+				u = u - v;
+				r = r - s;
 			}
 			else {
-				r = (r + p) / 2;
+				v = v - u;
+				s = s - r;
 			}
 		}
-		while ((v & 1) == 0) {
-			v = v / 2;
-			if ((s & 1) == 0) {
-				s = s / 2;
-			}
-			else {
-				s = (s + p) / 2;
-			}
-		}
-		if (u >= v) {
-			u = u - v;
-			r = r - s;
+		if (u == 1) {
+			*c =mod(r , p);
 		}
 		else {
-			v = v - u;
-			s = s - r;
+			*c =mod(s , p);
 		}
 	}
-	if (u == 1) {
-		**c= r % p;
-	}
-	else {
-		**c=s % p;
-	}
-	}
+	return 0;
 }
 
 
 void div_fp(const mpz_class& a, const mpz_class& b, const mpz_class& p, mpz_class* c)
 {
-	algo5_inv(b, p, &c);
+	//algo5_inv(b, p, c);
+	inv_fp(b, p, c);
 	mul_fp(a, *c, p, c);
 
 }
