04263a12685b245c2629e6df8291a3bc2dd26ec5
mimoex <nijidream@protonmail.com>
2022-07-01 17:41:22 +0900

    無限遠点の処理の修正

--- CSIDH/ec.cpp
+++ CSIDH/ec.cpp
@@ -1,6 +1,7 @@
 #include "fp.h"
 
-Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
+//Weierstrass Curve
+Point ec_add(const Point& p, const Point& q, const mpz_class& a, const mpz_class& mod)
 {
 	if (p.inf) return q;
 	if (q.inf) return p;
@@ -15,8 +16,9 @@ Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
 		//P + (-P) = 0
 		//cout << "Py,-Py:" << p.y << "," << -q.y << endl;
 		if ((p.y + q.y == mod) || (p.y == q.y && p.y == 0)) {
-			p.inf = true;
-			return p;
+			result.x = 0; result.y = 0;
+			result.inf = true;
+			return result;
 		}
 		//lambda=(3*x1*x1+a)/(2*y1)
 		mul_fp(p.x, p.x, mod, &lambda_temp);
@@ -53,10 +55,10 @@ Point ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& mod)
 
 
 //Montgomery Curve
-Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class& b, const mpz_class& mod)
+Point montgomery_ec_add(const Point& p, const Point& q, const mpz_class& a, const mpz_class& b, const mpz_class& mod)
 {
-	if (p.inf) return q;
-	if (q.inf) return p;
+	if (p.inf == 1) return q;
+	if (q.inf == 1) return p;
 
 	mpz_class x_temp, y_1, lambda;
 	mpz_class lh, rh;
@@ -68,8 +70,9 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 		//P + (-P) = 0
 		//cout << "Py,-Py:" << p.y << "," << -q.y << endl;
 		if ((p.y + q.y == mod) || (p.y == q.y && p.y == 0)) {
-			p.inf = true;
-			return p;
+			result.x = 0; result.y = 0;
+			result.inf = true;
+			return result;
 		}
 		//x1=x2のとき，
 		//
@@ -98,10 +101,16 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 		sub_fp(a3, a2, mod, &result.x);
 
 		//y
-		mpz_class temp10, temp11;
-		sub_fp(p.x, result.x, mod, &temp10);
-		mul_fp(lambda, temp10, mod, &temp11);
-		sub_fp(temp11, p.y, mod, &result.y);
+		mpz_class temp10, temp11,temp12, temp13,temp01, temp02, temp03, l3;
+		add_fp(a2, p.x, mod, &temp12);
+		add_fp(temp12, a, mod, &temp13);
+		mul_fp(temp13, lambda, mod, &temp01);
+
+		pow_fp(lambda, 3, mod, &l3);
+		mul_fp(b, l3, mod, &temp02);
+		
+		sub_fp(temp01, temp02, mod, &temp10);
+		sub_fp(temp10, p.y, mod, &result.y);
 
 	}
 	else {
@@ -112,13 +121,13 @@ Point montgomery_ec_add(Point& p, Point& q, const mpz_class& a, const mpz_class&
 		sub_fp(q.x, p.x, mod, &rh);
 		div_fp(lh, rh, mod, &lambda);
 
-		mpz_class lambda2, temp1;
+		mpz_class lambda2, temp1,temp2, temp3;
 
 		pow_fp(lambda, 2, mod, &lambda2);
 		mul_fp(b, lambda2, mod, &temp1);
-		sub_fp(temp1, a, mod, &result.x);
-		sub_fp(result.x, p.x, mod, &result.x);
-		sub_fp(result.x, q.x, mod, &result.x);
+		add_fp(p.x, q.x, mod, &temp2);
+		sub_fp(temp1, temp2, mod, &temp3);
+		sub_fp(temp3, a, mod, &result.x);
 
 		// y3 = (2*x1+x2+a)*(y2-y1)/(x2-x1)-b*(y2-y1)^3/(x2-x1)^3-y1
 		//左辺
